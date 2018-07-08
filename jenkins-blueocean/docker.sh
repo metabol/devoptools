3a5fa919064d856c12f9893ec739129ca121bfef
skanet <sky.net@gmx.com>
2018-07-08 15:16:26 +0200

    updated jenkins/docker

--- jenkins-blueocean/docker.sh
+++ jenkins-blueocean/docker.sh
@@ -1,4 +1,8 @@
-docker run -dit --name jenkins-blue-con \
+docker run -dit --name jenkinsblue-con \
+  --rm \
+  -u root \
   -p 8083:8080 \
   -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME":/home \
   jenkinsci/blueocean
